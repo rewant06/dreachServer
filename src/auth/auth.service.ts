8ac41c3d0fc48e3d167ed80dbd1a68c93baee6a5
Ritesh Jain <ritesh.vru@gmail.com>
2025-04-23 13:49:35 +0530

    refactor ♻️: Improved authentication service
    - Refactoring of authentication service was made to improve access and refresh token management with adjustments to expiration times.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,73 +5,94 @@ import { compare } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { response } from 'express';
 
-const expiresIn =60* 60 * 1000 * 24;
+const expiresIn = 60 * 60 * 1000 * 24;
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private JwtService: JwtService,
+  ) {}
 
-    constructor(private readonly userService:UserService,private JwtService:JwtService){}
+  // async login(dto:LoginDTO){
 
-    // async login(dto:LoginDTO){
+  //     const user = await this.validateUser(dto);
+  //     const payload = {
+  //         user,
+  //         sub:{
+  //             name:user.name,
+  //         }
+  //     }
 
-    //     const user = await this.validateUser(dto);
-    //     const payload = {
-    //         user,
-    //         sub:{
-    //             name:user.name,
-    //         }
-    //     }
+  //    return {
+  //         user,
+  //         backendToken:{
+  //             accessToken:await this.JwtService.signAsync(payload,{
+  //                 //expiresIn 5 MINUTES,
+  //                 expiresIn:"1d",
 
-    //    return {
-    //         user,
-    //         backendToken:{
-    //             accessToken:await this.JwtService.signAsync(payload,{
-    //                 //expiresIn 5 MINUTES,
-    //                 expiresIn:"1d",
-                
-    //                secret:process.env.ACCESS_TOKEN_SECRET
-    //             }),
+  //                secret:process.env.ACCESS_TOKEN_SECRET
+  //             }),
 
-    //             refreshToken:await this.JwtService.signAsync(payload,{
-    //                 expiresIn:"7d",
-    //                 secret:process.env.REFRESH_TOKEN_SECRET
-    //             }),
-    //             expiresIn:new Date().setTime(new Date().getTime()+expiresIn)
+  //             refreshToken:await this.JwtService.signAsync(payload,{
+  //                 expiresIn:"7d",
+  //                 secret:process.env.REFRESH_TOKEN_SECRET
+  //             }),
+  //             expiresIn:new Date().setTime(new Date().getTime()+expiresIn)
 
-    //         }
-    //     }
+  //         }
+  //     }
 
-    // }
+  // }
 
-    // async validateUser(dto:LoginDTO){
+  // async validateUser(dto:LoginDTO){
 
-    //     const user = await this.userService.findUserByEmail(dto.email);
-    //     if(user && (await compare(dto.password,user.password))){
-    //         const {password,...result} = user;
-    //         return result;
-    //     }
+  //     const user = await this.userService.findUserByEmail(dto.email);
+  //     if(user && (await compare(dto.password,user.password))){
+  //         const {password,...result} = user;
+  //         return result;
+  //     }
 
-    //     throw new UnauthorizedException('Invalid credentials');        
+  //     throw new UnauthorizedException('Invalid credentials');
 
-    // }
+  // }
 
-    // async refreshToken(user:any){
-    //     console.log("fired");
-    //     const payload={
-    //         user:user.user,
-    //         sub:user.sub
-    //     }
+  // async refreshToken(user:any){
+  //     console.log("fired");
+  //     const payload={
+  //         user:user.user,
+  //         sub:user.sub
+  //     }
 
-    //     return {
-    //         accessToken:await this.JwtService.signAsync(payload,{
-    //             expiresIn:"1d",
-    //             secret:process.env.ACCESS_TOKEN_SECRET
-    //         }),
-    //         refreshToken:await this.JwtService.signAsync(payload,{
-    //             expiresIn:"7d",
-    //             secret:process.env.REFRESH_TOKEN_SECRET
-    //         }),
-    //         expiresIn:new Date().setTime(new Date().getTime()+expiresIn)
-    //     }
-    // }
+  //     return {
+  //         accessToken:await this.JwtService.signAsync(payload,{
+  //             expiresIn:"1d",
+  //             secret:process.env.ACCESS_TOKEN_SECRET
+  //         }),
+  //         refreshToken:await this.JwtService.signAsync(payload,{
+  //             expiresIn:"7d",
+  //             secret:process.env.REFRESH_TOKEN_SECRET
+  //         }),
+  //         expiresIn:new Date().setTime(new Date().getTime()+expiresIn)
+  //     }
+  // }
+
+  async accessToken(user: any) {
+    const payload = {
+      user: user.user,
+      sub: user.sub,
+    };
+
+    return {
+      accessToken: await this.JwtService.signAsync(payload, {
+        expiresIn: '5m',
+        secret: process.env.ACCESS_TOKEN_SECRET,
+      }),
+      refreshToken: await this.JwtService.signAsync(payload, {
+        expiresIn: '7d',
+        secret: process.env.REFRESH_TOKEN_SECRET,
+      }),
+      expiresIn: new Date().setTime(new Date().getTime() + expiresIn),
+    };
+  }
 }
